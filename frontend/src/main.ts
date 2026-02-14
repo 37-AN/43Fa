e85a4da5cf62c3d44c34b503a891cff20578d925
43v3r Tech <159187089+37-AN@users.noreply.github.com>
2026-02-14 22:01:07 +0000

    Refactor ECharts import and setup for improved clarity and functionality

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,15 +3,17 @@ import App from './App.vue'
 import router from './router'
 
 // ECharts runtime setup (renderer + charts + components)
-import * as echarts from 'echarts/core'
-import { CanvasRenderer } from 'echarts/renderers'
-import { LineChart } from 'echarts/charts'
-import { TooltipComponent, LegendComponent, GridComponent } from 'echarts/components'
+import * as echarts from 'echarts'
 import VChart from 'vue-echarts'
-
-echarts.use([CanvasRenderer, LineChart, TooltipComponent, LegendComponent, GridComponent])
 
 const app = createApp(App)
 app.use(router)
 app.component('v-chart', VChart)
+// provide echarts instance for vue-echarts and make available globally
+app.provide('echarts', echarts)
+app.config.globalProperties.$echarts = echarts
+// also expose on window for any third-party code expecting a global
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+window.echarts = echarts
 app.mount('#app')
